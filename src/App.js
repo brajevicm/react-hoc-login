9ef0b4a1b768f54dc27167e129b7b741dff2de20
brajevicm <milos.brajevic@hotmail.com>
2018-04-04 16:37:24 +0200

    Implemented basic Auth HOC without Route HOC.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React, {Component, Fragment} from 'react';
-import {Route} from "react-router-dom";
+import React, { Component, Fragment } from "react";
+import { Route } from "react-router-dom";
 
 import HomePage from "./app/pages/HomePage";
 import ProfilePage from "./app/pages/ProfilePage";
 import LoginPage from "./app/pages/LoginPage";
-import GuestRoute from "./app/routes/GuestRoute";
-import UserRoute from "./app/routes/UserRoute";
+import withAuth from "./app/hoc/withAuth";
+import withoutAuth from "./app/hoc/withoutAuth";
 
 class App extends Component {
-
-    render() {
-        return (
-            <Fragment>
-                <Route exact path="/" component={HomePage} />
-                <GuestRoute
-                    exact
-                    path="/login"
-                    component={LoginPage}
-                />
-                <UserRoute
-                    exact
-                    path="/profile"
-                    component={ProfilePage}
-                />
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      <Fragment>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={withoutAuth(LoginPage)} />
+        <Route exact path="/profile" component={withAuth(ProfilePage)} />
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/app/hoc/withoutAuth.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { userService } from "../../service/UserService";
+
+export default ComposedComponent =>
+  class Auth extends Component {
+    componentDidMount = () => {
+      if (userService.isAuthenticated()) this.props.history.push("/");
+    };
+
+    render() {
+      return <ComposedComponent {...this.props} />;
+    }
+  };
